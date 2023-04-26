8db8910a3dafc7ff4ae6a3fc91d54853c73fbb75
DrKun <1315385515@qq.com>
2023-04-26 08:02:00 +0800

    初始化工程

---
+++ WeatherDemo/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+/*
+* ****************************************************************
+*
+* 文件名称 : AppDelegate
+* 作   者 : Created by 李亚坤
+* 创建时间 : 2023/4/26 07:59
+* 文件描述 : 
+* 注意事项 : 
+* 版权声明 : 
+* 修改历史 : 2023/4/26 初始版本
+*
+* ****************************************************************
+*/
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ WeatherDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+/*
+* ****************************************************************
+*
+* 文件名称 : ViewController
+* 作   者 : Created by 李亚坤
+* 创建时间 : 2023/4/26 07:59
+* 文件描述 : 
+* 注意事项 : 
+* 版权声明 : 
+* 修改历史 : 2023/4/26 初始版本
+*
+* ****************************************************************
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
